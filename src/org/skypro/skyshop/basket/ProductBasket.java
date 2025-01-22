69ade3f464e9ae78d997d1ab251cdc50e3c16854
Leroy <LeraRu@tut.by>
2025-01-23 01:39:45 +0300

    Introduction to OOP. Encapsulationмиs is done

---
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -0,0 +1,55 @@
+package org.skypro.skyshop.basket;
+
+import org.skypro.skyshop.product.Product;
+
+import java.util.Arrays;
+
+public class ProductBasket {
+    private Product [] basket = new Product [5];
+    private int productCount = 0;
+
+    public void addProduct (Product product) {
+        if (productCount < basket.length) {
+            basket[productCount] = product;
+            productCount++;
+        } else {
+            System.out.println("Невозможно добавить продукт!");
+        }
+    }
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (int i = 0; i < productCount; i++) {
+            totalPrice += basket[i].getPrice();
+        }
+        return totalPrice;
+    }
+
+    public void printBasket () {
+        if (productCount == 0) {
+            System.out.println("в корзине пусто");
+        } else {
+            for (int i = 0; i < productCount; i++) {
+                System.out.println(basket[i].getName() + ": " + basket[i].getPrice());
+            }
+            System.out.println("Итого: " + getTotalPrice());
+        }
+    }
+
+        public boolean isProductInBasket (String productName) {
+            for (int i = 0; i < productCount; i++) {
+                if (basket[i].getName().equals(productName)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        public void clearBasket() {
+            for (int i = 0; i < productCount; i++) {
+                basket[i] = null;
+            }
+            productCount = 0;
+        }
+
+}
---
+++ src/org/skypro/skyshop/product/Product.java
@@ -0,0 +1,24 @@
+package org.skypro.skyshop.product;
+
+
+public class Product {
+    private String name;
+    private int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
+
